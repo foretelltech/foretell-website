1b7179752ccf48d648f45aa8a0d336ccf1761148
Camilo Barraza <acordeonlwebmaster@gmail.com>
2020-03-29 12:46:29 -0500

    update footer links

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -2,10 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import AppContainer from './appContainer'
 import logoImg from '../../assets/logo.svg'
+import { Link } from 'gatsby'
 import googleImg from '../../assets/google-cloud-white.png'
 
 const Background = styled.div`
-   /* background-image: linear-gradient(to bottom, #1d1c55 219%, #050824 30%); */
   background-image: linear-gradient(to top, #050824,#0E0F36);
 `
 
@@ -33,16 +33,16 @@ const Wrapper = styled.div`
 `
 
 const Clickable = styled.div`
-  -webkit-touch-callout: none;
-  -webkit-user-select: none;
-  -khtml-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  :hover {
-    cursor: pointer;
-    opacity: 0.7;
-  }
+      -webkit-touch-callout: none;
+      -webkit-user-select: none;
+      -khtml-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      :hover {
+        cursor: pointer;
+        opacity: 0.7;
+      }
 `
 
 const Container = styled.div`
@@ -81,10 +81,36 @@ const Footer = () => {
               <div className='ml-5'>
                 <Title> Company </Title>
                 <Content>
-                  <Clickable>Foretell </Clickable>
-                  <Clickable>MP-MVP</Clickable>
-                  <Clickable>About Us</Clickable>
-                  <Clickable>Privacy Policy</Clickable>
+                  <Clickable>
+                    <Link
+                      to='/'
+                      style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                      }}>
+                      Foretell
+                    </Link>
+                  </Clickable>
+                  <Clickable>
+                    <Link
+                      to='/mvp'
+                      style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                      }}>
+                      MP-MVP
+                    </Link>
+                  </Clickable>
+                  <Clickable>
+                    <Link
+                      to='/aboutUs'
+                      style={{
+                        color: `white`,
+                        textDecoration: `none`,
+                      }}>
+                      About Us
+                    </Link>
+                  </Clickable>
                 </Content>
               </div>
             </Section>
